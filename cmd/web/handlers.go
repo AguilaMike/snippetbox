dfcff1d9ac84668d5598f196bc80943df8832bb3
David Hernandez <davidmiguelh@gmail.com>
2024-08-29 01:11:51 -0600

    adding ui

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +14,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// the second parameter is the header value.
 	w.Header().Add("Server", "Go")
 
-	w.Write([]byte("Hello from Snippetbox"))
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	// Use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. Notice that we use ... to pass the contents
+	// of the files slice as variadic arguments.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 // Add a snippetView handler function.
@@ -50,4 +77,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 // Add a snippetCreatePost handler function.
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Save a new snippet..."))
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./ui/static/file.zip")
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,16 @@ func main() {
 	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
 
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	// Prefix the route patterns with the required HTTP method (for now, we will
 	// restrict all three routes to acting on GET requests).
 	mux.HandleFunc("GET /{$}", home)                      // Restrict this route to exact matches on / only.
