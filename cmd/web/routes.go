81ad6ab67fa1bec19067ec3d5a144ffc32a72c83
David Hernandez <davidmiguelh@gmail.com>
2024-09-03 10:50:34 -0600

    third middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes.
 // Update the signature for the routes() method so that it returns a
@@ -28,10 +32,10 @@ func (app *application) routes() http.Handler {
 	// Create the new route, which is restricted to POST requests only.
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	// Pass the servemux as the 'next' parameter to the commonHeaders middleware.
-	// Because commonHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	// Wrap the existing chain with the logRequest middleware.
-	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
